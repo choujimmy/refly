cff1537bca768903c9fff00c76da189ad2465d7b
Jimmy Chou <jimmy@tdtztech.com>
2016-11-21 19:37:04 +0800

    增加admin首页

--- src/browser/routes/App.jsx
+++ src/browser/routes/App.jsx
@@ -1,24 +1,28 @@
 /* @flow */
 import React, { Component } from 'react'
 import CSSModules from 'react-css-modules'
-import { Match } from 'react-router'
+import { Match, MatchGroup } from 'react-router'
 import style from './style.scss'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Home from './home'
 import Register from './register'
 import Login from './login'
+import Admin from './admin'
 
 class App extends Component {
 
   render () {
     return (
       <div>
-        <Header />
-        <Match pattern='/' exactly component={Home} />
-        <Match pattern='/register' component={Register} />
-        <Match pattern='/login' component={Login} />
-        <Footer />
+        <div>
+          <Header />
+          <Match pattern='/' exactly component={Home} />
+          <Match pattern='/register' component={Register} />
+          <Match pattern='/login' component={Login} />
+          <Footer />
+        </div>
+        <Match pattern='/admin' component={Admin} />
       </div>
     )
   }
--- src/common/reducers/runtime/reducer.js
+++ src/common/reducers/runtime/reducer.js
@@ -2,16 +2,14 @@
 import { createReducer } from '../../core/createReducer'
 
 export const SET_INITIAL_NOW = 'runtime/SET_INITIAL_NOW'
-export const SET_AVAILABLE_LOCALES = 'runtime/SET_AVAILABLE_LOCALES'
 
 const initialState = {}
 
 export default createReducer(initialState, {
   [SET_INITIAL_NOW]: (state, action) => {
-    return state.set('initialNow', action.payload)
-  },
-
-  [SET_AVAILABLE_LOCALES]: (state, action) => {
-    return state.set('availableLocales', action.payload)
+    return {
+      ...state,
+      initialNow: action.payload
+    }
   }
 })
